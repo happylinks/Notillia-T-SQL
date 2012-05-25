d45e7578cd2bb780fe8ce48ba2954648ba8c2151
Bas <SM.vanderzandt@student.han.nl>
2012-05-25 12:51:54 +0200

    Added new datatypes + bugfixes + documentation

--- MySQL genereren/Genererate_Table.sql
+++ MySQL genereren/Genererate_Table.sql
@@ -16,7 +16,7 @@ Views needs to be generated before executing this script
 /* SP: the starting point for generating the MySql script												*/
 /*==============================================================*/
 
-ALTER PROC generateMySQL
+CREATE PROC generateMySQL
 
 @outputPath VARCHAR(MAX) = 'C:',
 @fileName VARCHAR(MAX) = 'mySql.sql'
@@ -33,7 +33,7 @@ SELECT @CreateDatabase += sub.Clause
 FROM (SELECT CASE t.[Schema] WHEN 'dbo' THEN '' ELSE t.[Schema] + '.' END + t.[Database] AS 'Clause' FROM Notillia.Tables t UNION
 SELECT CASE v.[Schema] WHEN 'dbo' THEN '' ELSE v.[Schema] + '.' END + v.[Database] AS 'Clause' FROM Notillia.Views v) sub
 
-SET @generated += 'CREATE DATABASE `' +  @CreateDatabase + '`; ' + CHAR(10) + 'USE DATABASE `' + @CreateDatabase + '`;' + CHAR(10)
+SET @generated += 'CREATE DATABASE `' +  @CreateDatabase + '`; ' + CHAR(10) + 'USE `' + @CreateDatabase + '`;' + CHAR(10)
 SET @generated += Notillia.generateCreateTable();
 SET @generated += Notillia.createMysqlFkFile();
 PRINT @generated
@@ -57,7 +57,8 @@ AS BEGIN
   DECLARE @tableOutput VARCHAR(max)
  
    SET @tableOutput = (SELECT 'CREATE TABLE `' + t.Table_Name + '`( '
-    + CHAR(10) + Notillia.getColumnsFromTable(t.Table_Name) + CHAR(10) + Notillia.getTableConstraints(t.table_name) + CHAR(10) + '); ' + CHAR(10)
+    + CHAR(10) + Notillia.getColumnsFromTable(t.Table_Name) + CHAR(10) + 
+		Notillia.getTableConstraints(t.table_name) + CHAR(10) + '); ' + CHAR(10) + CHAR(10)
    FROM Notillia.Tables t 
    FOR XML PATH(''))
    
@@ -78,7 +79,7 @@ AS BEGIN
    	
 DECLARE @output VARCHAR(max);
    	
-SET @output = (SELECT ' `' + c.Column_Name + '` ' + notillia.convertDataType(@tablename, c.Column_Name) + ',' + CHAR(10)
+SET @output = (SELECT CHAR(9) + ' `' + c.Column_Name + '` ' + notillia.convertDataType(@tablename, c.Column_Name) + ',' + CHAR(10)
 FROM Notillia.Columns c
 WHERE Table_Name = @tablename
 FOR XML PATH(''))
@@ -94,7 +95,7 @@ FOR XML PATH(''))
  @param varchar(max) @columnName The column of the table
  @return varchar(max) The converted datatype
 ==============================================================*/
- ALTER FUNCTION Notillia.convertDataType(@tableName VARCHAR(MAX), @columnName VARCHAR(max))
+ CREATE FUNCTION Notillia.convertDataType(@tableName VARCHAR(MAX), @columnName VARCHAR(max))
  RETURNS VARCHAR(max) AS BEGIN
  
  DECLARE @output VARCHAR(max);
@@ -105,8 +106,24 @@ SET @output = (SELECT CASE Data_type
  WHEN 'numeric' THEN 'NUMERIC' + '(' + CAST(Numeric_Precision AS VARCHAR(max)) +  ', ' + CAST(numeric_scale AS VARCHAR(max)) + ')'
  WHEN 'char' THEN 'CHAR' + '(' + CAST(Character_Maximum_Length AS VARCHAR(max)) + ')'
  WHEN 'datetime' THEN 'DATETIME'
- ELSE  Data_Type + '-- datatype may not be converted correctly'
- END + Notillia.generateDefaults(@columnName) + CASE IS_NULLABLE WHEN 'NO' THEN ' NOT NULL' ELSE ' NULL' END
+ WHEN 'int' THEN 'INT'
+ WHEN 'varBinary' THEN 'VARBINARY' + '(' + CAST(Character_Maximum_Length AS VARCHAR(max)) + ')'
+ WHEN 'tinyInt' THEN 'TINYINT'
+ WHEN 'smallint' THEN 'SMALLINT'
+ WHEN 'bigint' THEN 'BIGINT'
+ WHEN 'float' THEN 'FLOAT' + '(' + CAST(Numeric_Precision AS VARCHAR(max)) + ')'
+ WHEN 'decimal' THEN 'DECIMAL' + '(' + CAST(Numeric_Precision AS VARCHAR(max)) +  ', ' + CAST(numeric_scale AS VARCHAR(max)) + ')'
+ WHEN 'datetime2' THEN 'DATETIME'
+ WHEN 'date' THEN 'DATE'
+ WHEN 'time' THEN 'TIME'
+ WHEN 'binary' THEN 'BINARY'
+ WHEN 'nchar' THEN 'CHAR' + '(' + CAST(Character_Maximum_Length AS VARCHAR(max)) + ')' + ' CHARSET utf8'
+ WHEN 'money' THEN 'DECIMAL' + '(' + CAST(Numeric_Precision AS VARCHAR(max)) +  ', 2)'
+ WHEN 'smallmoney' THEN 'DECIMAL' + '(' + CAST(Numeric_Precision AS VARCHAR(max)) +  ', 2)'
+ WHEN 'real' THEN 'REAL'
+ 
+ ELSE  Data_Type + '/* datatype may not be converted correctly */'
+ END + Notillia.generateDefaults(@columnName, @tableName) + CASE IS_NULLABLE WHEN 'NO' THEN ' NOT NULL' ELSE ' NULL' END
  FROM Notillia.COLUMNS 
  WHERE Table_Name = @tableName AND Column_Name = @columnName
  FOR XML PATH(''))
@@ -209,6 +226,12 @@ END
    
 GO
 
+/*==============================================================
+ UDF: Returns a string including the primary and alternative
+ keys of the specified table.
+ @param varchar(max) @tableName The specified table
+ @return varchar(max) The primary and alternative keys in a String
+==============================================================*/
 CREATE FUNCTION Notillia.getTableConstraints(@tableName VARCHAR(max))
 RETURNS VARCHAR(MAX)
 AS BEGIN
@@ -228,14 +251,20 @@ RETURN @Output;
 END
 GO
 
-CREATE FUNCTION Notillia.generateDefaults(@ColumnName NVARCHAR(MAX))
+/*==============================================================
+ UDF: Generate a default parameter when the column has an default value
+ @param nvarchar(max) @tableName The table of the column
+ @param nvarchar(max) @columnName The column of the table
+ @return nvarchar(max) A default parameter string with the default value
+==============================================================*/
+CREATE FUNCTION Notillia.generateDefaults(@ColumnName NVARCHAR(MAX), @tableName NVARCHAR(MAX))
 RETURNS NVARCHAR(MAX)
 AS BEGIN
 DECLARE @output NVARCHAR(MAX)
 
 SET @output = (select ' DEFAULT ' +  RIGHT(LEFT(column_default , LEN(column_default) - 1), LEN(column_default) - 2)
 from notillia.columns
-WHERE column_Name = @ColumnName
+WHERE column_Name = @ColumnName AND table_name = @tableName
 for XML PATH(''))
 
 RETURN @output
@@ -243,22 +272,6 @@ END
 GO
 
 
-/* WORK IN PROGRESS
-CREATE FUNCTION generateIdentity(@columnName NVARCHAR(MAX))
-RETURNS NVARCHAR(MAX)
-AS BEGIN
-
-DECLARE @output NVARCHAR(MAX)
-
-SET @output = (select ' AUTO-INCREMENT '
-from Notillia.Columns
-where (columnproperty(object_id(table_name), column_name,'IsIdentity') = 1)
-AND Column_Name = @columnName)
-
-RETURN @output
-END
-GO
-*/
 
 
 /*==============================================================*/
@@ -305,7 +318,7 @@ AS
 BEGIN
 	DECLARE @String NVARCHAR(MAX); SET @String = '';
 	SELECT	@String += 'ALTER TABLE `' + FK.Master_Table + '`' + CHAR(10) + 
-				CHAR(9) + ' ADD CONSTRAINT ' + FK.Constraint_Name + ' FOREING KEY (' + Notillia.fnGetMasterColumnsForForeignKey (FK.[Schema], FK.Constraint_Name) + ') ' + CHAR(10) + 
+				CHAR(9) + ' ADD CONSTRAINT ' + FK.Constraint_Name + ' FOREIGN KEY (' + Notillia.fnGetMasterColumnsForForeignKey (FK.[Schema], FK.Constraint_Name) + ') ' + CHAR(10) + 
 					CHAR(9) + CHAR(9) + 'REFERENCES `' + FK.Child_Table + '` (' + Notillia.fnGetChildColumnsForForeignKey (FK.[Schema], FK.Constraint_Name) + ')' + CHAR(10) + 
 						CHAR(9) + CHAR(9) + CHAR(9) + ' ON CASCADE ' + FK.Update_Rule + CHAR(10) + 
 						CHAR(9) + CHAR(9) + CHAR(9) + ' ON DELETE	' + FK.Delete_Rule + CHAR(10) + CHAR(10)
